b16b60cbbbd43d0e38cd488f39cf489be4ef4f90
Jonathan Hamilton <jonathanjhamilton@gmail.com>
2026-08-03 23:52:34 -0400

    removed old test script

--- test.ts
+++
@@ -1,8 +0,0 @@
-function testFn(input: string): string
-function testFn(input?: undefined): undefined
-
-function testFn(input?: string): string | undefined {
-  return input
-}
-
-const test = testFn()

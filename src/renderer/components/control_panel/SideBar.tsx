94bb6e4d391edd4560795e74c7ebdcc5ec8241d5
Jonathan Hamilton <jonathanjhamilton@gmail.com>
2026-08-07 22:49:53 -0400

    refactor: simplify SideBar component by removing unused imports and options

--- src/renderer/components/control_panel/SideBar.tsx
+++ src/renderer/components/control_panel/SideBar.tsx
@@ -1,31 +1,13 @@
-import React, { use } from 'react'
+import React from 'react'
 
-import { FIT_TYPE } from '../../constants'
-
-import { ResponseContext } from '../../context/ResponseContext'
-
-import ToggleComponent from '../utility_components/ToggleComponent'
 import FitOptions from './FitOptions'
-import AlignmentOptions from './AlignmentOptions'
-import BackgroundColorOptions from './BackgroundColorOptions'
-import BackgroundOpacityOptions from './BackgroundOpacityOptions'
+
 import SaveOptions from './SaveOptions'
 
 export default function SideBar() {
-  const { fitType, isVertical } = use(ResponseContext)
-  const isContain = fitType === FIT_TYPE.CONTAIN
-
   return (
     <div className="sidebar">
-      <FitOptions>
-        <ToggleComponent shouldShow={isContain || fitType === FIT_TYPE.COVER}>
-          <AlignmentOptions isVertical={isVertical} />
-        </ToggleComponent>
-        <ToggleComponent shouldShow={isContain}>
-          <BackgroundColorOptions />
-          <BackgroundOpacityOptions />
-        </ToggleComponent>
-      </FitOptions>
+      <FitOptions />
       <SaveOptions />
     </div>
   )

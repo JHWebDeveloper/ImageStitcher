4fb26b10b5c5f92dcefffc786bfc4ff9718c55b4
Jonathan Hamilton <jonathanjhamilton@gmail.com>
2026-08-07 22:50:02 -0400

    feat: add RadioSet component for dynamic radio button selection

---
+++ src/renderer/components/utility_components/RadioSet.tsx
@@ -0,0 +1,44 @@
+import React, { type Dispatch, type PropsWithChildren, type SetStateAction, useId } from 'react'
+import { assertsIsKeyInObject } from '../../utilities'
+
+interface Props<T> extends PropsWithChildren {
+  name: string
+  label?: string
+  value?: T
+  onChange: Dispatch<SetStateAction<T | undefined | null>> | Function
+  optionLabels: Record<string, string>
+  optionValues: Record<string, T>
+}
+
+export default function RadioSet<T>({
+  name,
+  label,
+  value,
+  onChange,
+  optionLabels,
+  optionValues,
+  children
+}: Props<T>) {
+  const id = useId()
+
+  return (
+    <fieldset name={name}>
+      {label ? <legend>{label}</legend> : <></>}
+      {Object.entries(optionValues).map(([ key, val ], i) => {
+        assertsIsKeyInObject(key, optionLabels)
+
+        return (
+          <label key={`${id}_${i}`}>
+            <input
+              type="radio"
+              name={name}
+              checked={value === val}
+              onChange={() => onChange(val)}/>
+              {optionLabels[key]}
+          </label>
+        )
+      })}
+      { children }
+    </fieldset>
+  )
+}

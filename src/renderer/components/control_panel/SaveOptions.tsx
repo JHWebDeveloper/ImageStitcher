08b4ba9ca32f46d519c768413a8a58c25c0fc35a
Jonathan Hamilton <jonathanjhamilton@gmail.com>
2026-08-07 22:49:46 -0400

    refactor: reorganize SaveOptions component layout and improve label clarity

--- src/renderer/components/control_panel/SaveOptions.tsx
+++ src/renderer/components/control_panel/SaveOptions.tsx
@@ -8,6 +8,7 @@ import { ResponseContext } from '../../context/ResponseContext'
 import { SaveContext } from '../../context/SaveContext'
 
 import ToggleComponent from '../utility_components/ToggleComponent'
+import RadioSet from '../utility_components/RadioSet'
 import Select from '../utility_components/Select'
 import IconButton from '../utility_components/IconButton'
 import Checkbox from '../utility_components/Checkbox'
@@ -36,12 +37,44 @@ export default function SaveOptions() {
     [POST_SAVE_ACTION.NONE]: LABEL.NONE,
     [POST_SAVE_ACTION.LOAD_RESULT]: LABEL.LOAD_RESULT,
     [POST_SAVE_ACTION.CLEAR_BOTH]: LABEL.CLEAR_BOTH,
-    [POST_SAVE_ACTION.CLEAR_A]: `${LABEL.CLEAR} ${labelA}`,
-    [POST_SAVE_ACTION.CLEAR_B]: `${LABEL.CLEAR} ${labelB}`
+    [POST_SAVE_ACTION.CLEAR_A]: `${LABEL.CLEAR} ${labelA} ${LABEL.IMAGE}`,
+    [POST_SAVE_ACTION.CLEAR_B]: `${LABEL.CLEAR} ${labelB} ${LABEL.IMAGE}`
   }
 
   return (
     <div className="save-options">
+      <Select
+        label={LABEL.SAVE_FORMAT}
+        value={format}
+        onChange={saveCtx.setFormat}
+        optionLabels={LABEL}
+        optionValues={FORMAT} />
+      <RadioSet
+        name="save-type"
+        label={"Save Actions"}
+        value={saveType}
+        onChange={saveCtx.setSaveType}
+        optionLabels={saveTypeLabels}
+        optionValues={saveTypeValues}>
+        <ToggleComponent shouldShow={imageAHasOriginal && (isNewFile || saveType === SAVE_TYPE.REPLACE_B)}>
+          <Checkbox
+            label={`${LABEL.DELETE_ORIGINAL} ${labelA} ${LABEL.IMAGE}`}
+            checked={saveCtx.deleteA}
+            onChange={() => saveCtx.toggleDeleteA()} />
+        </ToggleComponent>
+        <ToggleComponent shouldShow={imageBHasOriginal && (isNewFile || saveType === SAVE_TYPE.REPLACE_A)}>
+          <Checkbox
+            label={`${LABEL.DELETE_ORIGINAL} ${labelB} ${LABEL.IMAGE}`}
+            checked={saveCtx.deleteB}
+            onChange={() => saveCtx.toggleDeleteB()} />
+        </ToggleComponent>
+      </RadioSet>
+      <Select
+        label={LABEL.POST_SAVE_ACTION}
+        value={saveCtx.postSaveAction}
+        onChange={saveCtx.setPostSaveAction}
+        optionLabels={postSaveActionLabels}
+        optionValues={POST_SAVE_ACTION} />
       <fieldset>
         <Checkbox
           label={LABEL.SAVE_ON_DROP}
@@ -52,45 +85,18 @@ export default function SaveOptions() {
           checked={saveCtx.shouldWarn}
           onChange={saveCtx.toggleShouldWarn} />
       </fieldset>
-      <Select
-        value={format}
-        onChange={saveCtx.setFormat}
-        optionLabels={LABEL}
-        optionValues={FORMAT} />
-      <Select
-        value={saveType}
-        onChange={saveCtx.setSaveType}
-        optionLabels={saveTypeLabels}
-        optionValues={saveTypeValues} />
-      <fieldset name="delete-options">
-        <ToggleComponent shouldShow={imageAHasOriginal && (isNewFile || saveType === SAVE_TYPE.REPLACE_B)}>
-          <Checkbox
-            label={`${LABEL.DELETE_ORIGINAL} ${labelA}`}
-            checked={saveCtx.deleteA}
-            onChange={() => saveCtx.toggleDeleteA()} />
-        </ToggleComponent>
-        <ToggleComponent shouldShow={imageBHasOriginal && (isNewFile || saveType === SAVE_TYPE.REPLACE_A)}>
-          <Checkbox
-            label={`${LABEL.DELETE_ORIGINAL} ${labelB}`}
-            checked={saveCtx.deleteB}
-            onChange={() => saveCtx.toggleDeleteB()} />
-        </ToggleComponent>
-      </fieldset>
-      <Select
-        value={saveCtx.postSaveAction}
-        onChange={saveCtx.setPostSaveAction}
-        optionLabels={postSaveActionLabels}
-        optionValues={POST_SAVE_ACTION} />
-      <IconButton
-        icon="cell_merge"
-        title={LABEL.FLATTEN}
-        onClick={() => flattenImage(format)}
-        disabled={!isImageALoaded} />
-      <IconButton
-        icon="save"
-        title={LABEL.SAVE_IMAGE}
-        onClick={() => saveImage(getSaveOptions())}
-        disabled={!isImageALoaded} />
+      <div className="save-buttons">
+        <IconButton
+          icon="cell_merge"
+          title={LABEL.FLATTEN}
+          onClick={() => flattenImage(format)}
+          disabled={!isImageALoaded} />
+        <IconButton
+          icon="save"
+          title={LABEL.SAVE_IMAGE}
+          onClick={() => saveImage(getSaveOptions())}
+          disabled={!isImageALoaded} />
+      </div>
     </div>
   )
 }

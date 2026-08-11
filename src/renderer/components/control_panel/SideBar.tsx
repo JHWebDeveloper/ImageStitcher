225d7353fba87a60a61dc815e9f32a42fd7de6d4
Jonathan Hamilton <jonathanjhamilton@gmail.com>
2026-08-10 21:00:15 -0400

    refactor: enhance SideBar component with layout toggle functionality for improved user interaction

--- src/renderer/components/control_panel/SideBar.tsx
+++ src/renderer/components/control_panel/SideBar.tsx
@@ -1,14 +1,26 @@
 import React from 'react'
 
+import { LABEL } from '../../constants'
+import { UseToggleDispatch } from '../../types'
+
 import FitOptions from './FitOptions'
-
 import SaveOptions from './SaveOptions'
+import IconButton from '../utility_components/IconButton'
 
-export default function SideBar() {
+interface Props {
+  isLeftLayout: boolean,
+  toggleIsLeftLayout: UseToggleDispatch
+}
+
+export default function SideBar({ isLeftLayout, toggleIsLeftLayout }: Props) {
   return (
     <div className="sidebar">
       <FitOptions />
       <SaveOptions />
+      <IconButton
+        icon={`dock_to_${isLeftLayout ? 'left' : 'right'}`}
+        title={isLeftLayout ? LABEL.MOVE_TO_RIGHT : LABEL.MOVE_TO_LEFT}
+        onClick={() => toggleIsLeftLayout()} />
     </div>
   )
 }

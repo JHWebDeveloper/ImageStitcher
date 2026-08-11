0da81c297ce35aad04d17a0c5b1cc58d3aef0813
Jonathan Hamilton <jonathanjhamilton@gmail.com>
2026-08-10 22:26:18 -0400

    refactor: enhance LayoutContext with initial state and toggle functionality

--- src/renderer/context/LayoutContext.tsx
+++ src/renderer/context/LayoutContext.tsx
@@ -1,20 +1,34 @@
 import React, { type PropsWithChildren, createContext } from 'react'
 
-import { LABEL_LR_VALUES, LABEL_TB_VALUES } from '../constants'
+import { DEFAULT_VALUE, LABEL_LR_VALUES, LABEL_TB_VALUES } from '../constants'
+import { useToggle } from '../hooks'
+import { UseToggleDispatch } from '../types'
 
 interface Props extends PropsWithChildren {
   isVertical: boolean
 }
 
-type LayoutContext = typeof LABEL_LR_VALUES | typeof LABEL_TB_VALUES
+type LayoutContext = {
+  isLeftLayout: boolean
+  toggleIsLeftLayout: UseToggleDispatch
+} & (typeof LABEL_LR_VALUES | typeof LABEL_TB_VALUES)
 
-export const LayoutContext = createContext<LayoutContext>(LABEL_LR_VALUES)
+const initState = {
+  isLeftLayout: DEFAULT_VALUE.LEFT_ALIGNED,
+  toggleIsLeftLayout() {},
+  ...LABEL_LR_VALUES
+}
+
+export const LayoutContext = createContext<LayoutContext>(initState)
 
 export function LayoutContextProvider({ isVertical, children }: Props) {
+  const [ isLeftLayout, toggleIsLeftLayout ] = useToggle(initState.isLeftLayout)
   const labels = isVertical ? LABEL_TB_VALUES : LABEL_LR_VALUES
 
   return (
     <LayoutContext value={{
+      isLeftLayout,
+      toggleIsLeftLayout,
       ...labels
     }}>
       { children }

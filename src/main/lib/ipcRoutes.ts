398e25aafdb3dcfe38a9753aeb30dfad247da3f3
Jonathan Hamilton <jonathanjhamilton@gmail.com>
2026-08-18 21:16:42 -0400

    fix: refactor IPC handling to use type-safe utilities for improved type safety and organization

--- src/main/lib/ipcRoutes.ts
+++ src/main/lib/ipcRoutes.ts
@@ -1,59 +1,18 @@
-import { ipcMain, type IpcMainEvent, type IpcMainInvokeEvent } from 'electron'
-
 import { CHANNEL, ERROR_MSG } from '../constants'
-import type { ChannelsWithoutPayload, ChannelsWithPayload, IpcChannel, IpcPayload, IpcResponse, PossiblePromise } from '../types'
+import type { IpcEvent } from '../types'
+import { ipcMainTypeSafe } from '../utilities'
 
 import type { ImageStitchData } from './uploadImages'
 
-function setListener<K extends keyof IpcChannel>(
-	channel: K,
-	callback: K extends ChannelsWithPayload
-		? (evt: IpcMainEvent, opts: IpcChannel[K]['payload']) => void
-		: (evt: IpcMainEvent) => void
-) {
-	ipcMain.on(channel, callback)
-}
+const { setListener, setHandler, send } = ipcMainTypeSafe
 
-type HandlerResponse<K extends keyof IpcChannel> = PossiblePromise<IpcResponse<K>> | Promise<void>
-
-function setHandler<K extends keyof IpcChannel>(
-	channel: K,
-	callback: K extends ChannelsWithPayload
-		? (evt: IpcMainInvokeEvent, opts: IpcChannel[K]['payload']) => HandlerResponse<K>
-		: (evt: IpcMainInvokeEvent) => HandlerResponse<K>
-) {
-	ipcMain.handle(channel, callback)
-}
-
-// Overload Signature
-function send<K extends ChannelsWithPayload>(
-	evt: IpcMainEvent | IpcMainInvokeEvent,
-	channel: K,
-	payload: IpcChannel[K]['payload']
-): void
-
-// Overload Signature
-function send<K extends ChannelsWithoutPayload>(
-	evt: IpcMainEvent | IpcMainInvokeEvent,
-	channel: K
-): void
-
-// Implmentation Signature
-function send<K extends keyof IpcChannel>(
-	evt: IpcMainEvent | IpcMainInvokeEvent,
-	channel: K,
-	payload?: IpcPayload<K>
-) {
-	evt.sender.send(channel, payload)
-}
-
-function sendErrorMessage(evt: IpcMainEvent | IpcMainInvokeEvent, message: string, cause: unknown) {
+function sendErrorMessage(evt: IpcEvent, message: string, cause: unknown) {
 	console.error(cause)
 	send(evt, CHANNEL.DISPLAY_ERROR_MESSAGE, new Error(message, { cause }))
 }
 
 export function setIpcRoutes(imageStitcher: ImageStitchData) {
-	async function sendStitchResult(evt: IpcMainEvent | IpcMainInvokeEvent) {
+	async function sendStitchResult(evt: IpcEvent) {
 		try {
 			send(evt, CHANNEL.DISPLAY_STITCH_RESULT, {
 				result: await imageStitcher.getPreviewState(),

2fa391a6824b00175488c762d2d2f02f113c6646
Jonathan Hamilton <jonathanjhamilton@gmail.com>
2026-08-22 02:43:00 -0400

    refactor: remove unused Props interface and provider component in ElectronAPIContext

--- src/renderer/context/ElectronAPIContext.tsx
+++ src/renderer/context/ElectronAPIContext.tsx
@@ -1,17 +1,7 @@
-import React, { createContext, type PropsWithChildren } from 'react'
+import { createContext } from 'react'
 
 import { NAMESPACE } from '../constants'
-
-interface Props extends PropsWithChildren {}
 
 const { electronAPI } = window[NAMESPACE]
 
 export const ElectronAPI = createContext(electronAPI)
-
-export function ElectronAPIProvider({ children }: Props) {
-	return (
-		<ElectronAPI value={electronAPI}>
-			{ children }
-		</ElectronAPI>
-	)
-}
